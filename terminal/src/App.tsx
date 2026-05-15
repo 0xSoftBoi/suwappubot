8be404bcd4c1f76d93e5b58c2c00240643c7db7a
Eric Manganaro <ericmanganaro@gmail.com>
2026-05-15 13:38:03 -0400

    Apply Summer Breeze scope to terminal workspace

--- terminal/src/App.tsx
+++ terminal/src/App.tsx
@@ -6,6 +6,7 @@ import { PointsDashboard } from './components/points/PointsDashboard'
 import { HotkeysHelpOverlay } from './components/hotkeys/HotkeysHelpOverlay'
 import { SuwappuBotSiteReplacement } from './components/templates/SuwappuBotSiteReplacement'
 import { TerminalSiteReplacement } from './components/templates/TerminalSiteReplacement'
+import { TerminalThemeScope } from './theme/TerminalThemeScope'
 
 function isTerminalHost() {
   if (typeof window === 'undefined') return false
@@ -14,16 +15,18 @@ function isTerminalHost() {
 
 function TradingWorkspace() {
   return (
-    <div className="h-screen flex flex-col bg-terminal-bg text-terminal-text font-sans">
-      <Header />
-      <main className="flex-1 overflow-hidden">
-        <Routes>
-          <Route path="/points" element={<PointsDashboard />} />
-          <Route path="points" element={<PointsDashboard />} />
-          <Route path="*" element={<TradingLayout />} />
-        </Routes>
-      </main>
-    </div>
+    <TerminalThemeScope mode="summer-breeze">
+      <div className="h-screen flex flex-col bg-terminal-bg text-terminal-text font-sans">
+        <Header />
+        <main className="flex-1 overflow-hidden">
+          <Routes>
+            <Route path="/points" element={<PointsDashboard />} />
+            <Route path="points" element={<PointsDashboard />} />
+            <Route path="*" element={<TradingLayout />} />
+          </Routes>
+        </main>
+      </div>
+    </TerminalThemeScope>
   )
 }
 
