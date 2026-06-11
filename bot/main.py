05798a715c6fb49803ce9e6e5c7c08a9f2505ab2
0xSoftBoi <tsoma4770@gmail.com>
2026-06-11 19:29:46 -0400

    revert: remove foreign alert callback registrations from bot/main.py (alerts.py was reverted)

--- bot/main.py
+++ bot/main.py
@@ -101,13 +101,7 @@ from bot.handlers.admin_fees import (
 )
 
 # New handlers
-from bot.handlers.alerts import (
-    alerts_handler,
-    alert_conversation,
-    alerts_menu_callback,
-    alert_manage_callback,
-    alert_delete_callback,
-)
+from bot.handlers.alerts import alerts_handler, alert_conversation, alerts_menu_callback
 from bot.handlers.referral import (
     referral_handler,
     ref_menu_callback_handler,
@@ -414,8 +408,6 @@ def add_handlers(application: Application) -> None:
 
     # Alerts
     application.add_handler(CallbackQueryHandler(alerts_menu_callback, pattern="^alerts_menu$"))
-    application.add_handler(CallbackQueryHandler(alert_manage_callback, pattern="^alert_manage$"))
-    application.add_handler(CallbackQueryHandler(alert_delete_callback, pattern="^alert_delete_"))
 
     # Referrals & Fees
     application.add_handler(ref_menu_callback_handler)
