ae5a6b4af0af25e4e3eb5dcdd1bad06059b629ef
Tsolmondorj Natsagdorj <tsoma4770@gmail.com>
2026-08-21 00:36:58 -0400

    fix(primitives): include idle assets in ERC4626 adapter

--- contracts/primitives/strategies/ERC4626YieldStrategy.sol
+++ contracts/primitives/strategies/ERC4626YieldStrategy.sol
@@ -6,6 +6,7 @@ import {ISuwappuYieldStrategy} from "../interfaces/ISuwappuYieldStrategy.sol";
 interface IERC4626Asset {
     function balanceOf(address account) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
+    function transfer(address to, uint256 amount) external returns (bool);
 }
 
 interface IERC4626Target {
@@ -31,6 +32,7 @@ contract ERC4626YieldStrategy is ISuwappuYieldStrategy {
     error Unauthorized();
     error BadParams();
     error ApprovalFailed();
+    error TransferFailed();
     error SlippageExceeded();
     error AccountingMismatch();
 
@@ -54,25 +56,25 @@ contract ERC4626YieldStrategy is ISuwappuYieldStrategy {
     }
 
     function totalAssets() public view override returns (uint256) {
-        return target.convertToAssets(target.balanceOf(address(this)));
+        return underlying.balanceOf(address(this)) + target.convertToAssets(target.balanceOf(address(this)));
     }
 
     function liquidAssets() external view override returns (uint256) {
-        uint256 claim = totalAssets();
+        uint256 idle = underlying.balanceOf(address(this));
+        uint256 claim = target.convertToAssets(target.balanceOf(address(this)));
         uint256 maxNow = target.maxWithdraw(address(this));
-        return claim < maxNow ? claim : maxNow;
+        return idle + (claim < maxNow ? claim : maxNow);
     }
 
     function deposit(uint256 assets, bytes calldata) external override onlyVault returns (uint256 deployed) {
         if (assets == 0 || underlying.balanceOf(address(this)) < assets) revert AccountingMismatch();
-        uint256 beforeShares = target.balanceOf(address(this));
+        uint256 beforeAssets = totalAssets();
         _forceApprove(address(target), assets);
         target.deposit(assets, address(this));
         _forceApprove(address(target), 0);
-        uint256 afterShares = target.balanceOf(address(this));
-        if (afterShares < beforeShares) revert AccountingMismatch();
-        uint256 newValue = target.convertToAssets(afterShares - beforeShares);
-        deployed = newValue < assets ? newValue : assets;
+        uint256 afterAssets = totalAssets();
+        if (afterAssets < beforeAssets) revert AccountingMismatch();
+        deployed = assets;
     }
 
     function withdraw(uint256 assets, uint256 minAssetsOut, bytes calldata)
@@ -82,7 +84,11 @@ contract ERC4626YieldStrategy is ISuwappuYieldStrategy {
         returns (uint256 assetsOut)
     {
         uint256 beforeBal = underlying.balanceOf(vault);
-        target.withdraw(assets, vault, address(this));
+        uint256 idle = underlying.balanceOf(address(this));
+        uint256 fromIdle = idle < assets ? idle : assets;
+        if (fromIdle != 0 && !underlying.transfer(vault, fromIdle)) revert TransferFailed();
+        uint256 remaining = assets - fromIdle;
+        if (remaining != 0) target.withdraw(remaining, vault, address(this));
         uint256 afterBal = underlying.balanceOf(vault);
         if (afterBal < beforeBal) revert AccountingMismatch();
         assetsOut = afterBal - beforeBal;
@@ -95,8 +101,10 @@ contract ERC4626YieldStrategy is ISuwappuYieldStrategy {
         onlyVault
         returns (uint256 assetsOut)
     {
-        uint256 shares = target.balanceOf(address(this));
         uint256 beforeBal = underlying.balanceOf(vault);
+        uint256 idle = underlying.balanceOf(address(this));
+        if (idle != 0 && !underlying.transfer(vault, idle)) revert TransferFailed();
+        uint256 shares = target.balanceOf(address(this));
         if (shares != 0) target.redeem(shares, vault, address(this));
         uint256 afterBal = underlying.balanceOf(vault);
         if (afterBal < beforeBal) revert AccountingMismatch();
